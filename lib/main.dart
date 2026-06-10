0c8f1128fba9461d8f1029f32aecfe8842a36d1b
Hamza Wasiq <hamzawasiq79@gmail.com>
2026-06-10 18:32:42 +0500

    Today's work

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'alert_screen.dart';
+import 'notification_provider.dart';
 
-void main() => runApp(const RideLockrApp());
+void main() => runApp(
+  ChangeNotifierProvider(create: (_) => NotificationProvider(), child: const RideLockrApp()),
+);
