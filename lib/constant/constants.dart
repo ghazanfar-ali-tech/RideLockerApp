0b918411dc31d468c385b478dc3cdc320cedb163
Ahsan Sajjad <asajjad827@gmail.com>
2026-06-16 21:33:12 +0500

    remove constants file from github tracking

--- lib/constant/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static const String apiKey = "AIzaSyArKf6Vdh8h2aMUVuB1VFzzKtLHINPZNo0";
-  static const String appId = "1:836261461801:android:6d61c8a5c88a7d1164c138";
-  static const String messagingSenderId = "836261461801";
-  static const String projectId = "riderlocker-ba777";
-}
